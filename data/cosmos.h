3d3413026446e66dfd4903aed9713d7d9a228e9a
Sorina Lupu <lupusorina@yahoo.com>
2016-12-08 15:57:21 +0100

    image for background

---
+++ data/cosmos.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(cosmos)
+
+//======================================================================
+//
+//	cosmos, 256x256@8, 
+//	+ palette 256 entries, not compressed
+//	+ bitmap not compressed
+//	Total size: 512 + 65536 = 66048
+//
+//	Time-stamp: 2016-12-08, 15:45:18
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_COSMOS_H
+#define GRIT_COSMOS_H
+
+#define cosmosBitmapLen 65536
+extern const unsigned int cosmosBitmap[16384];
+
+#define cosmosPalLen 512
+extern const unsigned short cosmosPal[256];
+
+#endif // GRIT_COSMOS_H
+
+//}}BLOCK(cosmos)
